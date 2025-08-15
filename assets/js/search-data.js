fa512b19c2d020d976733eb1c578621253adc57c
Dineth5 <dineth330@gmail.com>
2025-08-15 18:43:41 +0000

    Deploying to gh-pages from @ Dineth5/Dineth5.github.io@c971c9f2c8e401d36382d4b44f44a60c4e2402f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-contact",
+          title: "Contact",
+          description: "Get in touch with me.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/contact/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
